e6d8f8e1eb69dc38d63b74b125bc2f8e8ae8e198
David Orozco <cdorozco74@misena.edu.co>
2023-08-08 09:20:43 -0500

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,14 @@ export class AppComponent {
   name = 'Tienda';
   edad = 18;
   img = "https://www.uplers.com/wp-content/uploads/2022/05/AngularJs-Frameworks.jpg"
+  btn = true;
+  persona = {
+    nombre: 'David',
+    apellido: 'Orozco',
+    age: 18,
+    avatar: 'https://pps.whatsapp.net/v/t61.24694-24/345356866_3402672116714511_8350754741430174530_n.jpg?ccb=11-4&oh=01_AdRkau8FbO82_lPQSVBQFpn5QPJDboIHAnrxfv2XkYEF0Q&oe=64DB8610'
+  }
+  
+
 }
+
